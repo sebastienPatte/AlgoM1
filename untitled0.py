e979d7ba9fe2378c58ae1dd2712594303b57706b
info121 <sebastien.patte@u-psud.fr>
2020-10-01 12:35:34 +0200

    graphe complet plus courts chemins entre terminaux

---
+++ untitled0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 09:41:26 2020
+
+@author: spatte
+
+Problème de Steiner
+
+Données :
+    - G(V, E) , w: E -> |N
+    - T inclu dans V, les terminaux du graphe 
+    
+Question :
+    G' = (V', E') , sous-graphe connexe (arbre)
+    T inclu dans V' et tq somme(w(a,b)) avec a,b appartiennent à E soit minimale
+"""
+
