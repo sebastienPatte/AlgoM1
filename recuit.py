6f125b45df4600f3477969c7aafed55272e1024b
sebastienPatte <sebnb91@gmail.com>
2020-11-13 10:57:20 +0100

    courbe résultats moyennés le long de l'exécution avec intervalle de confiance

--- recuit.py
+++ recuit.py
@@ -197,7 +197,7 @@ def drawPlot(names, values):
     plt.show()
 
 
-def computeInstance(path, graph, terms, T, Tmin, deltaT):
+def computeInstance(graph, terms, Tinit, Tmin, deltaT, path=None):
     
     NB_POINTS = 15
     
@@ -207,14 +207,22 @@ def computeInstance(path, graph, terms, T, Tmin, deltaT):
     interMax = []
     
     # on construit le nom du fichier en fonction des paramètres
-    strTmin = str(Tmin).split('.')[1]
-    strDeltaT = str(deltaT).split('.')[1]
-    path += "/"+str(T)+"_"+strTmin+"_"+strDeltaT
-    print(path)
-    # f = open(path,"a")
+    if (not (path is None)):
+        if not os.path.exists(path):
+            os.makedirs(path)
+        
+        strTmin = str(Tmin).split('.')[1]
+        strDeltaT = str(deltaT).split('.')[1]
+        path += "/"+str(Tinit)+"_"+strTmin+"_"+strDeltaT
+        print(path)
+        # test si le fichier n'existe pas déjà 
+        if os.path.isfile(path):
+            raise NameError("ComputeInstance : le fichier "+path+" existe déjà")
+        # ouverture fichier
+        f = open(path,"a")
     
     #remplissage keys et values avec la 1ère éxécution
-    k, v, r = recuit(graph, terms, T, Tmin, deltaT)
+    k, v, r = recuit(graph, terms, Tinit, Tmin, deltaT)
     
     
     #on récupère 10 points parmis ceux explorés
@@ -250,115 +258,108 @@ def computeInstance(path, graph, terms, T, Tmin, deltaT):
         # moyenne des valeurs entre old_xPt et xPt
         vals = vals / (xPt-old_xPt)
         values.append(vals)
-        
-        #maj interMin/Max pour avoir une position relative a vals et non absolue
-        interMax[cpt] = interMax[cpt] - vals
-        interMin[cpt] = vals - interMin[cpt]
-        
-        
+        #maj old_xPt et cpt
         old_xPt = xPt
         cpt+=1
     
     print("1 | val :",r)
     
-    #on ajoute les valeurs des autres éxécutions dans values
-    # for i in range(100):
-    #     # k : keys from recuit
-    #     # v : values from recuit
-    #     # r : result from recuit
-    #     k, v, r = recuit(graph, terms, T, Tmin, deltaT)
-    #     print(i+2, "| val :",r)    
-    #     # parcours des valeurs explorées par recuit
-    #     for j in keys:
-    #         values[j] += v[j] 
-    #         #maj val_min de l'intervalle
-    #         if(v[j] < interMin[j]):
-    #             interMin[j] = v[j]
-    #         #maj val_max de l'intervalle
-    #         if(v[j] > interMax[j]):
-    #             interMax[j] = v[j]
-                    
-    #calul des moyennes de valeurs
-    # for i in range(len(values)):
-    #     values[i] = values[i] / 100
+    # on ajoute les valeurs des autres éxécutions dans values
+    for i in range(99):
+        # k : keys from recuit
+        # v : values from recuit
+        # r : result val from recuit
+        k, v, r = recuit(graph, terms, Tinit, Tmin, deltaT)
+        print(i+2, "| val :",r)    
+        # on initialise values avec les valeurs de la 1ère exécution
+        cpt = 0
+        old_xPt = 0 
+        #on parcoure les 10 points sur l'axe x qui ont été choisis (xPt)
+        for xPt in keys:
+            vals = 0
+            #pour chaque xPt on parcoure les valeurs entre lui et l'ancien point (old_xPt)
+            for j in range(old_xPt, xPt):
+            
+                #maj intervalle de confiance
+                if(v[j] < interMin[cpt]):
+                    interMin[cpt] = v[j]
+                if(v[j] > interMax[cpt]):
+                    interMax[cpt] = v[j]
+                
+                vals+= v[j]
+        
+            # moyenne des valeurs entre old_xPt et xPt
+            vals = vals / (xPt-old_xPt)
+            # on ajoute la valeur moyennée pour cette courbe à celles des autre courbes
+            values[cpt] += (vals)
+            #maj old_xPt et cpt
+            old_xPt = xPt
+            cpt+=1
     
     
-    # f.write((str)(val)+"\n")
-    # f.close()
+    for cpt in range(len(values)):
+        # moyenne sur les 100 courbes
+        values[cpt] = values[cpt] / 100
+        #maj interMin/Max pour avoir une position relative a values et non absolue
+        interMax[cpt] = interMax[cpt] - values[cpt]
+        interMin[cpt] = values[cpt] - interMin[cpt]
+        # on enregistre les données calculées ligne par ligne (key val interMin interMax)
+        if (not (path is None)):
+            f.write(str(keys[cpt])+" "+str(values[cpt])+" "+str(interMin[cpt])+" "+str(interMax[cpt])+"\n")
     
-    
+    #fermeture fichier de données
+    if (not (path is None)):
+        f.close()
+    #dessin de la courbe avec les errorbars
     plt.plot(keys, values)
     plt.errorbar(keys, values, yerr=[interMin, interMax], fmt='none') #capsize=5
-    
     plt.show()
     
 
-def plotFromFile(path, Tinit, Tmin, deltaT):
+def plotFromFile(Tinit, Tmin, deltaT, path):
+    
+    strTmin = str(Tmin).split('.')[1]
+    strDeltaT = str(deltaT).split('.')[1]
+    path += "/"+str(Tinit)+"_"+strTmin+"_"+strDeltaT
+    print(path)
+    # test si le fichier existe 
+    if not os.path.isfile(path):
+        raise NameError("plotFromFile : le fichier "+path+" n'existe pas")
+    
+    # ouverture fichier
+    f = open(path,"r")
     
     # récupération des données
-    
-    f = open(path,"r")
-    cpt=0
-    names = []
+    keys = []
     values = []
+    interMin = []
+    interMax = []
     for l in f:
-        names.append(cpt)
-        val = (float)(l)
+        # on récupère les valeurs dans la ligne du fichier
+        lTab = l.split(" ")
+        key = int(lTab[0])
+        val = float(lTab[1])
+        iMin = float(lTab[2])
+        iMax = float(lTab[3])
+        # on remplit les tableaux
+        keys.append(key)
         values.append(val)
-        cpt+=1
+        interMin.append(iMin)
+        interMax.append(iMax)
+    # fermeture fichier
     f.close()
 
-    ecart_type = st.stdev(values)
-    moy = st.mean(values)
-    
-    #choix de l'intervalle de confiance
-    intervale = ecart_type
-    
-    # courbe principale
-    plt.plot(names,values)
-    
-    # Moyenne
-    plt.plot([0,100], [moy,moy], '--', color='green')
-    plt.text(101,moy,str(round(moy, 2)), color='green')
-    
-    # intervalle de confiance
-    uBound = str(round(moy+intervale, 2))
-    dBound = str(round(moy-intervale, 2))
-    plt.plot([0,100],[moy+intervale,moy+intervale], '--', color='red')
-    plt.plot([0,100],[moy-intervale,moy-intervale], '--', color='red')
-    plt.text(101, moy+intervale, uBound, color='red')
-    plt.text(101, moy-intervale, dBound, color='red')
-    
-    # plt.text(0,moy-intervale-4,"$-\sigma$")
-    
+
+    # courbe avec errorbars
+    plt.plot(keys,values)
+    plt.errorbar(keys, values, yerr=[interMin, interMax], fmt='none') #capsize=5
     
     # légendes et titre
-    plt.xlabel('number of runs')
-    plt.ylabel('solution evaluation')
-
-    # plt.legend(['courbe des résultats', 'moyenne', 'intervalle de confiance ($\sigma$)'], bbox_to_anchor=(1,1), loc='upper left')
-    
+    plt.xlabel('number of iterations')
+    plt.ylabel('average solution evaluation')
     plt.title("Estimations avec $T_{init}="+ str(Tinit) +"$ $T_{min}="+ str(Tmin) +"$ et $\Delta_{T}="+ str(deltaT) +"$")
-
-    # plt.text(0.92,0.61,"Points dans l'intervalle \nde confiance :"+str(inInter)+" %", transform=plt.gcf().transFigure)
-    # plt.text(0.92,0.49,"$\sigma =$"+str(intervale), transform=plt.gcf().transFigure)
-    # plt.text(0.92,0.44,"- "+str(moy-intervale), transform=plt.gcf().transFigure)
-    # plt.text(0.92,0.40,"+ "+str(moy+intervale), transform=plt.gcf().transFigure)
-    
-    plt.margins(x=0.01)
+    # dessin
     plt.show()
-    
-    # Prints
-    
-    inInter = 0
-    for i in values:
-        if(i>=moy-intervale and i<=moy+intervale):
-            inInter+=1
-    
-    print("écart type :", ecart_type)
-    print("moyenne:", moy)
-    print("dans l'intervale :",inInter,"%")
-    print()
     
     
 if __name__ == "__main__":
@@ -369,41 +370,13 @@ if __name__ == "__main__":
     # print(eval_sol(graph,terms,sol))
     
     
-    with open("test.stp") as my_file:
+    with open("B/b03.stp") as my_file:
         my_parser = SteinlibParser(my_file, my_class)
         my_parser.parse()
         terms = my_class.terms
         graph = my_class.my_graph
         
-        # print_graph(graph,terms)
-        # sol=approx_steiner(graph,terms)
-        # print(sol)
-        # print_graph(graph,terms,sol)
-        # print(eval_sol(graph,terms,sol))
-        
-        
         # print("sumW",(graph.size(weight="weight")))
         
-        # recuit(graph, terms, 1000, 0.1, 0.999)
-        # computeInstance("newNeigh/450000_1_999", graph, terms, 550000, 0.1, 0.999)
-        # plotFromFile("newNeigh/450000_1_999", 550000, 0.1, 0.999)
-        
-        # computeInstance("newNeigh/455_11_999", graph, terms, 455, 0.11, 0.999)
-        # plotFromFile("newNeigh/455_1_999", 455, 0.1, 0.999)
-     
-        # plotFromFile("newNeigh/100000_1_999", 100000, 0.1, 0.999)
-        # plotFromFile("newNeigh/10000_1_997", 10000, 0.1, 0.997)
-        # plotFromFile("newNeigh/10000_1_998", 10000, 0.1, 0.998)
-        # plotFromFile("newNeigh/10000_1_999", 10000, 0.1, 0.999)
-        # plotFromFile("newNeigh/1000_1_999", 1000, 0.1, 0.999)
-        # plotFromFile("newNeigh/454_1_999", 454, 0.1, 0.999)
-        # plotFromFile("newNeigh/455_1_999", 455, 0.1, 0.999)
-        # plotFromFile("newNeigh/456_1_999", 456, 0.1, 0.999)
-        
-        # computeInstance("b02/10000_1_999", graph, terms, 10000, 0.1, 0.999)
-        # plotFromFile("b02/10000_1_999", 10000, 0.1, 0.999)
-        
-        # computeInstance("new_b02", graph, terms, 4550, 0.01, 0.999)
-        # plotFromFile("b02/4550_01_999", 4550, 0.01, 0.999)
-     
-        computeInstance("test", graph, terms, 1000, 0.1, 0.99)
+        computeInstance(graph, terms, 5555, 0.1, 0.999, "b03")
+        plotFromFile(5555, 0.1, 0.999, "b03")
